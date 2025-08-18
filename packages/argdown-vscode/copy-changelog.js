1c73b62eb25ce2acbf34db7c731e5a46b160d533
Hatim Shabbir <hatim.shabbir5152@gmail.com>
2025-08-18 20:26:12 +0200

    fix(argdown-vscode): Removed broken test suites and script

--- packages/argdown-vscode/copy-changelog.js
+++ packages/argdown-vscode/copy-changelog.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
 // destination.txt will be created or overwritten by default.
-fs.copyFile("../../docs/changes/README.md", "CHANGELOG.md", err => {
+fs.copyFile("../../packages/argdown-docs/docs/changes/README.md", "CHANGELOG.md", err => {
   if (err) throw err;
 });
