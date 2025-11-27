2637618192b159a25b0bf9426f540750553494e1
Lucas Wiese <lucas.wiese@gmx.de>
2025-11-27 17:08:37 +0100

    import web-comonent via package dependency. Remove old vuepress files
    We should import the argdown-map web component as a dependency -> Don't
    use some post build scripts for copying files. Technical migration is
    done. Need to focus on styles now.

--- packages/argdown-docs/docs/.vitepress/theme/index.ts
+++ packages/argdown-docs/docs/.vitepress/theme/index.ts
@@ -4,12 +4,16 @@ import { inBrowser, type Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 
 // Web Component, required by the @argdown/markdown-it-plugin. But we can not import it from there because VitePress blocks it. Therefore create the plugin without importing the script and import the web component here. See also config.mts
+<<<<<<< HEAD
 // This is only imported in the browser, because it uses "document" to define the custom elements.
 if (inBrowser) {
   // @ts-ignore: TODO: Refactor web-components to module
   import("@argdown/web-components");
   // import("@argdown/web-components/style.css");
 }
+=======
+import "@argdown/web-components";
+>>>>>>> b791a04 (import web-comonent via package dependency. Remove old vuepress files)
 
 // Vue Components
 import ArgdownCheatSheet from "../components/ArgdownCheatSheet.vue";
