9572c93282a5b03b00cf3e1d9bbb0268852ac603
Kushal12341997 <kushal972021@gmail.com>
2026-01-31 13:51:43 +0100

    supressing some linter warnings, fixing some other warnings in language-server

--- packages/argdown-language-server/src/providers/DocumentSymbolPlugin.ts
+++ packages/argdown-language-server/src/providers/DocumentSymbolPlugin.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 import { DocumentSymbol, Range, SymbolKind } from "vscode-languageserver";
 // import Uri from "vscode-uri";
 import {
--- packages/argdown-language-server/src/providers/FoldingRangesPlugin.ts
+++ packages/argdown-language-server/src/providers/FoldingRangesPlugin.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 import { FoldingRange } from "vscode-languageserver";
 import {
   IArgdownPlugin,
