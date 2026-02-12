fe464ad58517fbc8205f2d9feb912b34f0cddf6c
Lucas Wiese <lucas.wiese@gmx.de>
2026-02-12 19:14:04 +0100

    [sandbox] add prettier plugin to eslint
    Signed-off-by: Lucas Wiese <lucas.wiese@gmx.de>

--- packages/argdown-sandbox/eslint.config.js
+++ packages/argdown-sandbox/eslint.config.js
@@ -1,10 +1,12 @@
 import pluginVue from "eslint-plugin-vue";
 import globals from "globals";
 import js from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   ...pluginVue.configs["flat/recommended"],
   js.configs.recommended,
+  eslintPluginPrettierRecommended,
   {
     rules: {
       "no-console": "off",
