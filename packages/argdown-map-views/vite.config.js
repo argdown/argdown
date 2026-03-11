9ffc0252b11554459abc5e81f0edbcdfb627c321
Lucas Wiese <lucas.wiese@gmx.de>
2026-03-11 15:57:03 +0100

    [map-views] update fileName remove old dev deps
    Signed-off-by: Lucas Wiese <lucas.wiese@gmx.de>

--- packages/argdown-map-views/vite.config.js
+++ packages/argdown-map-views/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: packageName,
-      fileName: "index"
+      fileName: "argdown-map"
     }
   }
 });
