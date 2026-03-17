a3c530e36d066516d1e6f6a9ed9e9daf455b34f3
Lucas Wiese <lucas.wiese@gmx.de>
2026-03-17 18:08:50 +0100

    [language server] update test server dest
    Signed-off-by: Lucas Wiese <lucas.wiese@gmx.de>

--- packages/argdown-language-server/test/LanguageServerTestHelper.ts
+++ packages/argdown-language-server/test/LanguageServerTestHelper.ts
@@ -28,7 +28,7 @@ export class LanguageServerTestHelper {
   private initializeCompleted = false;
 
   async startServer(): Promise<void> {
-    const serverPath = path.join(__dirname, "../dist/server.js");
+    const serverPath = path.join(__dirname, "../dist/server.node.cjs");
 
     this.serverProcess = spawn("node", [serverPath, "--stdio"], {
       stdio: ["pipe", "pipe", "pipe"]
