8166ece7f9f8acb779078dd313f39cdb7df34c6b
Lucas Wiese <lucas.wiese@gmx.de>
2026-07-24 21:42:11 +0200

    [language server] comile argdown with @include that is grammar sensitive

--- packages/argdown-language-server/src/CompileArgdown.ts
+++ packages/argdown-language-server/src/CompileArgdown.ts
@@ -1,4 +1,8 @@
+import { argdown } from "@argdown/core";
+
 export class CompileArgdown {
+
+  private argdown = argdown;
   /**
    * Loads a file from the given path, relative to the `from` path.
    * @param path The relative or absolute path of the file to load.
@@ -8,7 +12,7 @@ export class CompileArgdown {
   private loadfile: (path: string, from: string) => Promise<[string, string]>;
   /**
    *
-   * @param loadfile see {@link CompilePlugin.loadfile}
+   * @param loadfile see {@link CompileArgdown.loadfile}
    */
   constructor(
     loadfile: (path: string, from: string) => Promise<[string, string]>
@@ -22,23 +26,19 @@ export class CompileArgdown {
     from: string,
     alreadyIncluded: Set<string> = new Set()
   ): Promise<string> {
-    const regex: RegExp = /@include\(([^)]+)\)/g;
-    const matches = Array.from(input.matchAll(regex));
-    if (matches.length === 0) return input;
+    const { includes } = this.argdown.run({
+      process: ["parse-input", "include-positions"],
+      input
+    });
+    if (!includes || includes.length === 0) return input;
     const loads = await Promise.all(
-      matches.map(async (match) => {
-        const name = match[1];
+      includes.map(async ({payload, startLine}) => {
+        const name: string = payload.filePath;
         let id, content;
         try {
           [id, content] = await this.loadfile(name, from);
         } catch {
-          const lineNumber =
-            input.split("\n").findIndex((line) => line.includes(name)) + 1;
-
-          return `<!-- File ${name} not found. Is included by ${from}:${lineNumber} -->`;
-          // throw new Error(
-          //   `File ${name} not found. Is included by ${from}:${lineNumber}.`
-          // );
+          return `<!-- File ${name} not found. Is included by ${from}:${startLine} -->`;
         }
         if (!id || !content) return `<!-- Empty content: ${name} -->`;
         if (alreadyIncluded.has(id)) {
@@ -48,6 +48,9 @@ export class CompileArgdown {
         return `<!-- Start: ${id} -->\n${await this.compile(content, id, alreadyIncluded)}\n<!-- End: ${id} -->`;
       })
     );
-    return input.replace(regex, () => loads.shift() || "");
+    loads.forEach((load, i) => {
+      input = input.replace(includes[i].image, load);
+    });
+    return input;
   }
 }
